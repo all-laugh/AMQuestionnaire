872c1699b51488c40170219030146731d89206c2
Xiao Quan 全笑 <58939349+all-laugh@users.noreply.github.com>
2022-03-03 10:52:59 -0500

    Initial Commit

---
+++ Questionnaire/QuestionnaireApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuestionnaireApp.swift
+//  Questionnaire
+//
+//  Created by Xiao Quan on 3/3/22.
+//
+
+import SwiftUI
+
+@main
+struct QuestionnaireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
